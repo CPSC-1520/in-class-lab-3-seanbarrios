db1029b568b3f6d4e4cd26c7560534fca9d061e0
Sean Barrios <sbarrios1@nait.ca>
2024-10-02 21:17:16 -0600

    declared consts

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 // Enter your code below.
 const showResourcesButton = document.getElementById('show-resources');
+const resourcesList = document.querySelector('.javascript-resources');
+const listItems = document.querySelectorAll('.list-group-item');
+
