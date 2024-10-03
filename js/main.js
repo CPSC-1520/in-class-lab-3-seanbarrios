7dc589590495689119ce6e4522aed77ef6cff220
Sean Barrios <sbarrios1@nait.ca>
2024-10-02 21:21:07 -0600

    functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 // Enter your code below.
+
+// selectors
 const showResourcesButton = document.getElementById('show-resources');
 const resourcesList = document.querySelector('.javascript-resources');
 const listItems = document.querySelectorAll('.list-group-item');
 
+// show / hide resources
+function toggleResources() {
+    resourcesList.classList.toggle('d-none');
+  }
+
+// bold
+function boldHoveredItem(event) {
+    event.target.style.fontWeight = 'bold';
+  }
