d4568204dd7df2b6b8471a3604f2ede06e8873b7
Sean Barrios <sbarrios1@nait.ca>
2024-10-02 21:30:47 -0600

    fixed syntax

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 // Enter your code below.
 
-// selectors
+// Selectors
 const showResourcesButton = document.getElementById('show-resources');
 const resourcesList = document.querySelector('.javascript-resources');
 const listItems = document.querySelectorAll('.list-group-item');
 
-// show / hide resources
+// Show/hide resources function
 function toggleResources() {
-    resourcesList.classList.toggle('d-none');
-  }
+  resourcesList.classList.toggle('d-none');
+}
 
-// bold
+// Bold function
 function boldHoveredItem(event) {
-    event.target.style.fontWeight = 'bold';
-  }
+  event.target.style.fontWeight = 'bold';
+}
 
-// remove bold 
+// Remove bold function
 function unboldHoveredItem(event) {
-    event.target.style.fontWeight = 'normal';
-  }
+  event.target.style.fontWeight = 'normal';
+}
 
-// italicize clicked item 
+// Italicize clicked item function
 function italicizeClickedItem(event) {
-    event.target.style.fontStyle = 'italic';
-  }
+  event.target.style.fontStyle = 'italic';
+}
 
 // new event listener, toggle resources when clicked
 showResourcesButton.addEventListener('click', toggleResources);
 
-// add event listeners to each list item
+// add event listeners to each item
 listItems.forEach(item => {
-    // hover event to bold text
-    item.addEventListener('mouseover', boldHoveredItem);
-    
-    // mouseout event to revert to normal text
-    item.addEventListener('mouseout', unboldHoveredItem);
-    
-    // click event to italicize text
-    item.addEventListener('click', italicizeClickedItem);
-  });
+  // hover event to bold text
+  item.addEventListener('mouseover', boldHoveredItem);
+  
+  // mouseout event to revert to normal text
+  item.addEventListener('mouseout', unboldHoveredItem);
+  
+  // click event to italicize text
+  item.addEventListener('click', italicizeClickedItem);
+});
